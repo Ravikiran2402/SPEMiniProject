8370e435585caf5d332ec0fe67e7a7c9ba66e6e4
Ravikiran2402 <ravikiranbudde@gmail.com>
2021-03-10 16:02:51 +0530

    added complete packaging

--- src/main/ui/vite.config.js
+++ src/main/ui/vite.config.js
@@ -1,23 +1,7 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// const proxyPlugin = require('vite-plugin-proxy')
-// https://vitejs.dev/config/
 export default {
   plugins: [
     vue(),
-    // proxyPlugin({
-    //   '/api': {
-    //     target: 'http://localhost:9999',
-    //     changeOrigin: true,
-    //     onProxyRes: (proxyRes) => {
-    //       // cache all responses for faster development
-    //       // use browser's "Disable cache" in dev tools when you need to update API responses
-    //       proxyRes.headers['Cache-Control'] = `public, max-age=${ONE_YEAR_IN_SECONDS}`
-    //       // delete headers you don't want
-    //       delete proxyRes.headers['expires']
-    //     },
-    //   },
-    // }),
   ],
 }
