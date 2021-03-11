ac519c63a31da6079d2b928291a44c258bd18491
Ravikiran2402 <ravikiranbudde@gmail.com>
2021-03-11 15:34:28 +0530

    added more tests

--- src/test/java/spe/scientificcalc/ScientificcalcApplicationTests.java
+++ src/test/java/spe/scientificcalc/ScientificcalcApplicationTests.java
@@ -10,22 +10,42 @@ class ScientificcalcApplicationTests {
     Calculator c = new Calculator();
 
     @Test
-    public void testPower() {
+    public void testPower_truePos() {
         Assertions.assertEquals(125, c.power(5,3), 0);
     }
 
     @Test
-    public void testFactorial() {
+    public void testFactorial_truePos() {
         Assertions.assertEquals(120, c.factorial(5), 0);
     }
 
     @Test
-    public void testLog() {
+    public void testLog_truePos() {
         Assertions.assertEquals(2.63905732 , c.naturalLog(14), 0.1);
     }
 
     @Test
-    public void testRoot() {
+    public void testRoot_truePos() {
         Assertions.assertEquals(4, c.squareRoot(16), 0);
+    }
+
+    @Test
+    public void testPower_trueNeg() {
+        Assertions.assertNotEquals(126, c.power(5,3), 0);
+    }
+
+    @Test
+    public void testFactorial_trueNeg() {
+        Assertions.assertNotEquals(140, c.factorial(5), 0);
+    }
+
+    @Test
+    public void testLog_trueNeg() {
+        Assertions.assertNotEquals(2.63905732 , c.naturalLog(15), 0.1);
+    }
+
+    @Test
+    public void testRoot_trueNeg() {
+        Assertions.assertNotEquals(4, c.squareRoot(17), 0);
     }
 }
