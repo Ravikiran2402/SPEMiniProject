e97158b475d0b22b7db18accb80aa3f391b6b170
Ravikiran2402 <ravikiranbudde@gmail.com>
2021-03-13 21:44:11 +0530

    fixed logging

--- src/main/java/spe/scientificcalc/Calculator.java
+++ src/main/java/spe/scientificcalc/Calculator.java
@@ -1,34 +1,52 @@
 package spe.scientificcalc;
 
 import java.lang.Math;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Calculator {
+    private final static Logger logger = LogManager.getLogger();
+    
     public double squareRoot(double n) {
+        logger.info("[SQRT_FUNC - Request] : "+n);
         if (n < 0) {
+            logger.info("[SQRT_FUNC - Invalid Request] : "+n);
             throw new ArithmeticException(n + " is negative");
         }
-        return Math.sqrt(n);
+        double ret_val = Math.sqrt(n);
+        logger.info("[SQRT_FUNC - Response] : "+ret_val);
+        return ret_val;
     }
 
     public long factorial(int n) {
+        logger.info("[FACTORIAL_FUNC - Request] : "+n);
         if (n < 0) {
+            logger.info("[FACTORIAL_FUNC - Invalid Request] : "+n);
             throw new ArithmeticException(n + " is negative");
         }
         long fact = 1;
         for (int i = 1; i <= n; ++i) {
             fact *= i;
         }
+        logger.info("[FACTORIAL_FUNC - Response] : "+fact);
         return fact;
     }
 
     public double naturalLog(double x) {
+        logger.info("[LOG_FUNC - Request] : "+x);
         if (x < 0) {
+            logger.info("[LOG_FUNC - Invalid Request] : "+x);
             throw new ArithmeticException(x + " is negative");
         }
-        return Math.log1p(x);
+        double ret_val = Math.log1p(x);
+        logger.info("[LOG_FUNC - Response] : "+ret_val);
+        return ret_val;
     }
 
     public double power(double b, double e) {
-        return Math.pow(b,e);
+        logger.info("[POWER_FUNC - Request] : ("+b+","+e+")");
+        double ret_val = Math.pow(b,e);
+        logger.info("[POWER_FUNC - Response] : "+ret_val);
+        return ret_val;
     }
 }
